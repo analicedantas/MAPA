1503e18cde42042d3400dd3aa666a1f77af0e51f
Analice Dantas Araújo <150189303+analicedantas@users.noreply.github.com>
2025-09-08 10:28:47 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,18 +1,36 @@
 CREATE TABLE paciente(
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    endereco VARCHAR(50),
-    telefone VARCHAR(11),
-	nascimento DATE,
-    CPF VARCHAR(11),
+    nome_paciente VARCHAR(100),
+    telefone_paciente VARCHAR(11),
+    endereco_paciente VARCHAR(50),
+    nascimento_paciente DATE,
+    CPF_paciente VARCHAR(11) UNIQUE 
+);
+
+CREATE TABLE medico(
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    nome_medico VARCHAR(100),
+    especialidade_medico VARCHAR(100),
+    CPF_medico VARCHAR(11) UNIQUE,
+    CRM_medico VARCHAR(100) UNIQUE, 
+    telefone_medico VARCHAR(11),
+    endereco_medico VARCHAR(50)
 );
 
 CREATE TABLE consultas(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     data_consulta DATE,
     horario TIME,
-    CRM VARCHAR(100),
-    CPF VARCHAR(11),
-    FOREIGN KEY (CRM) REFERENCES medico(CRM),
-    FOREIGN KEY (CPF) REFERENCES paciente(CPF)
+    CPF_paciente VARCHAR(11),         
+    CRM_medico VARCHAR(100),          
+    FOREIGN KEY (CPF_paciente) REFERENCES paciente(CPF_paciente),
+    FOREIGN KEY (CRM_medico) REFERENCES medico(CRM_medico)        
 );
+
+
+
+
+
+
+
+
