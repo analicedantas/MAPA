5e216d71af097da92388654c45ef175ea6fc3b6c
Analice Dantas Araújo <150189303+analicedantas@users.noreply.github.com>
2025-09-18 09:52:30 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,1 +1,15 @@
+<?php
 
+$usuario = 'root';
+$senha = '';
+$dbname = 'dbmapa';
+$host = 'localhost';
+
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
+}
+?>
