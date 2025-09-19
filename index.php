115050938d47d66a16880ba8f553703ff4130be7
Analice Dantas Araújo <150189303+analicedantas@users.noreply.github.com>
2025-09-19 08:24:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,76 @@
-
+<?php
+require_once "config.php";
+ 
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Por favor coloque um nome de usuário.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "O nome de usuário pode conter apenas letras, números e sublinhados.";
+    } else{
+        $sql = "SELECT id FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            $param_username = trim($_POST["username"]);
+            
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    $username_err = "Este nome de usuário já está em uso.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+            unset($stmt);
+        }
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Por favor insira uma senha.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "A senha deve ter pelo menos 6 caracteres.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Por favor, confirme a senha.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "A senha não confere.";
+        }
+    }
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+         
+        if($stmt = $pdo->prepare($sql)){
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); 
+            
+            if($stmt->execute()){
+                header("location: login.php");
+            } else{
+                echo "Ops! Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+            unset($stmt);
+        }
+    }
+    
+    unset($pdo);
+}
+?>
      
 <!DOCTYPE html>
 <html lang="pt">
