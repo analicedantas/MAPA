fb1ea26b798119463ec27317f69ac21391d05641
Analice Dantas Araújo <150189303+analicedantas@users.noreply.github.com>
2025-10-14 07:31:37 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-  $conexao = mysqli_connect('localhost', 'root', '', 'sa');
+  $conexao = mysqli_connect('localhost', 'root', '', 'MAPA');
   if (!$conexao) {  
     die("Falha na conexao com o banco de dados: " . mysqli_connect_error());  
   }
